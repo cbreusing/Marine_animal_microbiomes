76c77d25e7431ebac4bc3840a2228688552d5935
cbreusing <77281303+cbreusing@users.noreply.github.com>
2022-04-04 08:30:44 -0400

    Add files via upload

--- 16S_rRNA_amplicon_analyses/06_Heatmap.r
+++ 16S_rRNA_amplicon_analyses/06_Heatmap.r
@@ -16,7 +16,7 @@ ann$Migration <- factor(ann$Migration, levels = c("Yes", "No", "Unknown"))
 
 ha <- HeatmapAnnotation(Migration = ann$Migration, Diet = ann$Diet, Depth = ann$Depth, annotation_height = unit(4, "mm"), col = list(Migration = c("Yes" = "paleturquoise4", "No" = "palevioletred4", "Unknown" = "darkgrey"), Diet = c("Detritus" = "sandybrown", "Phytoplankton" = "darkgreen", "Zooplankton" = "saddlebrown"), Depth = c("242" = "#eff3ff", "308" = "#bdd7e7", "452" = "#6baed6", "817-862" = "#2171b5", "1120-1150" = "#013270")))
 
-pdf("Function_potential_heatmap_full.pdf", width=9, height=8)
+pdf("Function_potential_heatmap_full.pdf", width=9, height=9)
 map1 <- Heatmap(mat1, width = unit(12, "cm"), height = unit(15, "cm"), top_annotation = ha, km = 1, name = "Abundance", col = colRamp, show_row_names = TRUE, row_names_side = "left", row_names_gp = gpar(fontsize = 7), cluster_rows = FALSE, clustering_distance_columns = "euclidean", clustering_method_columns = "complete", show_column_names = TRUE, column_names_gp = gpar(fontsize = 7, fontface = "bold.italic", col = c(rep("mistyrose", 3), rep("lightblue1", 3), "lightsteelblue2", rep("red1", 4), rep("pink2", 3), rep("royalblue1", 4), rep("lightgoldenrod1", 6), rep("maroon4", 4), rep("darkgoldenrod1", 2), rep("midnightblue", 2), rep("cyan4", 5))))
 draw(map1, annotation_legend_side = "right", merge_legend = TRUE)
 dev.off()
