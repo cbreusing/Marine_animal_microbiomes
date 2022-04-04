3b3e5eb4677e7ed2a76f45d8806d5f7b29ea011c
cbreusing <77281303+cbreusing@users.noreply.github.com>
2022-04-04 08:40:38 -0400

    Update 08_Paired_t-test.r

--- 16S_rRNA_amplicon_analyses/08_Paired_t-test.r
+++ 16S_rRNA_amplicon_analyses/08_Paired_t-test.r
@@ -1,6 +1,6 @@
 setwd("/Users/Corinna/Documents/Work/Beinart_Lab/Marine_animal_microbiomes_UCSD/16S_amplicons/microbiome")
 
-data <- read.table("Actinobacteria_mean_proportions.txt", header=T)
+data <- read.table("Actinobacteria_mean_proportions_filtered.txt", header=T)
 
 d <- with(data, Value[Group == "Metagenomics"] - Value[Group == "Amplicons"])
 shapiro.test(d)
